b2460d68380318b5e6e2a16cfd7ccd468d4d73c3
Felipe Bona <felipechiodinibona@hotmail.com>
2024-09-11 22:17:18 -0300

    ajusta

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -1,5 +1,33 @@
 <template>
-    <div>
-        pedidos
+  <div class="p-3">
+    <h1>Pedidos</h1>
+    <div class="d-flex flex-column">
+      <div class="border rounded p-3" v-for="(order, key) in orders" :key="key">
+        <span>#{{ order.id }}</span>
+        <span>{{ order.status }}</span>
+      </div>
     </div>
+  </div>
 </template>
+
+<script>
+  export default {
+    data: () => {
+      return {
+        orders: []
+      }
+    },
+    mounted() {
+      api.get('order')
+        .then(({ data }) => {
+          this.orders = data.orders
+        })
+        .catch(() => {
+          // this.$router.push({ name: 'home' })
+        })
+        .finally(() => {
+          this.loading = false  
+        })
+    }
+  }
+</script>
